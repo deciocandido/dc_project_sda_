489e4de290d19ef1d7b630709cdd1642a9ec8575
deciocandido <51098348+deciocandido@users.noreply.github.com>
2021-03-08 22:45:02 +0100

    taskClass implented interFace

--- taskClass.java
+++ taskClass.java
@@ -1,16 +1,32 @@
-import java.util.Date;
+public class taskClass implements taskInterface{
 
-public class taskClass {
-    private String taskTitle;
-    private Date taskDate;
-    private String taskState;
-    
+     String taskTitle;
+     String taskProject;
+     Boolean taskStatus;
 
-    public taskClass(String taskTitle, Date taskDate, String taskState) {
-        this.taskDate = taskDate;
-        this.taskState = taskState;
+    public taskClass(String taskTitle, String taskProject, Boolean taskStatus) {
         this.taskTitle = taskTitle;
+        this.taskProject = taskProject;
+        this.taskStatus = taskStatus;
     }
 
+    @Override
+    public void task_Title(String taskTitle) {
+        this.taskTitle = taskTitle;
+        System.out.println("This is the task Title: " + this.taskTitle);
+    }
 
+    @Override
+    public void task_Project(String taskProject){
+        this.taskProject = taskProject;
+        System.out.println("This is the task Project: " + this.taskTitle);
+    }
+
+    @Override
+    public void task_Status(Boolean taskStatus){
+        this.taskStatus = taskStatus;
+        System.out.println("This is the task Status: " + this.taskStatus);
+    }
+
+   
 }
